2c05ad27dcaeff6286dc1557d77d77df6ead9da7
KumaranByteCrafter <kumaranravi3112003@gmail.com>
2025-01-02 00:50:20 +0530

    test api data to store on db

--- api/index.php
+++ api/index.php
@@ -118,6 +118,26 @@
                 ];
                 $data=$this->json($data);
                 $this->response($data,200);
+                }
+        }
+        private function signup(){
+            if($this->get_request_method() == "POST" and  isset($this->_request['username']) and isset($this->_request['email']) and isset($this->_request['password'])){
+                $username = $this->_request['username'];
+                $email = $this->_request['email'];
+                $password = $this->_request['password'];
+                try{
+                    $s = new Signup($username,$password,$email);
+                    $data = [
+                        "message" => "Signup success",
+                        "userid" => $s->getInsertID(),
+                    ];
+                    $this->response($this->json($data),200);
+                }catch(Exception $e){
+                        $data = [
+                            "error" => $e->getMessage(),
+                        ];
+                        $this->response($this->json($data),200);
+                }
             }
         }
 
--- api/lib/Signup.class.php
+++ api/lib/Signup.class.php
@@ -1,6 +1,7 @@
 <?php
 require_once('Database.class.php');
 class Signup{
+    private $id;
     private $username;
     private $password;
     private $email;
@@ -10,9 +11,18 @@ class Signup{
         $this->username = $username;
         $this->password = $password;
         $this->email = $email;
+        $bytes = random_bytes(16);
+        $token = bin2hex($bytes);
+        $password = $this->hashPassword();
+        $query = "INSERT INTO `auth` (`username`, `password`, `email`, `active`, `token`) VALUES ('$username', '$password', '$email', 0, '$token');";
+        if(!mysqli_query($this->db,$query)){
+            throw new Exception("unable to signup");
+        }else{
+            $this->id = mysqli_insert_id($this->db);
+        }
     }
     public function getInsertID(){
-
+        return $this->id;
     }
     public function hashPassword(){
         $options = [
